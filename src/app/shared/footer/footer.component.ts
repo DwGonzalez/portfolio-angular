44e6dce2e698efcd9ded20386d688f70ecff3737
Darwin Gonzalez <8992534+DwGonzalez@users.noreply.github.com>
2019-04-09 21:50:25 -0400

    Fin Seccion 4

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PageInfoService } from 'src/app/services/page-info.service';
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  anio: number = new Date().getFullYear();
+  year: number = new Date().getFullYear();
 
-  constructor() { }
+  constructor(public _pageInfoService: PageInfoService) { }
 
   ngOnInit() {
   }
